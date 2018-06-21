01e08ac80c6b68db65e5050fe149211d0df826e0
wanpan <1622638366@qq.com>
2018-06-21 11:59:07 +0800

    增加数字滚动页面，但有报错信息，目前还用不了

--- src/components/number-flip.vue
+++ src/components/number-flip.vue
@@ -1,17 +1,43 @@
 <template>
   <div>
-
+    <div class="flip"></div>
+    <div class="num"></div>
+    <button @click="shuffle">click to shuffle</button>
   </div>
 </template>
 <script>
+import Flip from "number-flip";
+const $ = s => document.querySelector(s);
+var flip;
+var tick;
+
 export default {
-  name: 'number-flip',
-  data () {
+  name: "number-flip",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    shuffle() {
+      const num = ~~(Math.random() * 999);
+      $(".num").innerText = num;
+      flip.flipTo({ to: num, direct: true });
     }
+  },
+
+  mounted() {
+    flip = new Flip({
+      node: $(".flip"),
+      from: 725,
+      // easeFn: function(pos) {
+      //   if ((pos /= 0.5) < 1) return 0.5 * Math.pow(pos, 3);
+      //   return 0.5 * (Math.pow(pos - 2, 3) + 2);
+      // },
+      systemArr: ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"]
+    });
   }
-}
+};
 </script>
 
 
